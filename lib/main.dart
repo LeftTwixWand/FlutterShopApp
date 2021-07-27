c677d1cdf30df8c88477d2b99ebcd89d8c9c86ff
Vladyslav <lefttwixwand@gmail.com>
2021-07-27 22:58:25 +0300

    feat: add the cart provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import './providers/cart.dart';
 import './screens/product_detail_screen.dart';
 import './screens/product_overview_screen.dart';
 import './providers/products_provider.dart';
-import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (ctx) => ProductsProvider(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (ctx) => ProductsProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (ctx) => Cart(),
+        ),
+      ],
       child: MaterialApp(
         title: 'MyShop',
         theme: ThemeData(
---
+++ lib/providers/cart.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/foundation.dart';
+
+class CartItem {
+  final String id;
+  final String title;
+  final int quantity;
+  final double price;
+
+  CartItem(
+      {required this.id,
+      required this.title,
+      required this.quantity,
+      required this.price});
+}
+
+class Cart with ChangeNotifier {
+  var _items = new Map<String, CartItem>();
+
+  Map<String, CartItem> get items => {..._items};
+
+  void addItem(String productId, title, double price) {
+    if (_items.containsKey(productId)) {
+      _items.update(
+          productId,
+          (value) => CartItem(
+              id: value.id,
+              title: value.title,
+              quantity: value.quantity + 1,
+              price: value.price));
+    } else {
+      _items.putIfAbsent(
+        productId,
+        () => CartItem(
+            id: DateTime.now().toString(),
+            title: title,
+            quantity: 1,
+            price: price),
+      );
+    }
+  }
+}
