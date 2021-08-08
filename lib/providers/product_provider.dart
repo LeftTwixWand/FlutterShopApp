8255d134aa0a948c310c945a8a6d44e085932782
Vladyslav <lefttwixwand@gmail.com>
2021-08-08 23:01:57 +0300

    feat: Apdating favorite state on a server

--- lib/providers/product_provider.dart
+++ lib/providers/product_provider.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class ProductProvider with ChangeNotifier {
   final String id;
@@ -16,8 +19,32 @@ class ProductProvider with ChangeNotifier {
       required this.imageUrl,
       this.isFavorite = false});
 
-  void toggleFavoriteStatus() {
+  void _setFavoriteValue(bool newValue) {
+    isFavorite = newValue;
+    notifyListeners();
+  }
+
+  Future<void> toggleFavoriteStatus() async {
+    final oldStatus = isFavorite;
+
     isFavorite = !isFavorite;
     notifyListeners();
+
+    final url = Uri.parse(
+        'https://flutter-update-973d5-default-rtdb.europe-west1.firebasedatabase.app/products/$id.json');
+
+    try {
+      final response = await http.patch(
+        url,
+        body: json.encode(
+          {
+            'isFavorite': isFavorite,
+          },
+        ),
+      );
+      if (response.statusCode >= 400) _setFavoriteValue(oldStatus);
+    } catch (error) {
+      _setFavoriteValue(oldStatus);
+    }
   }
 }
