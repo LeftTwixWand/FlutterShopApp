80c653078704517abcea2086cc3b27d5ce7b63f3
Vladyslav <lefttwixwand@gmail.com>
2021-08-12 20:09:51 +0300

    feat: Added authorization screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import './screens/auth_screen.dart';
 import './screens/edit_product_screen.dart';
 import './screens/user_products_screen.dart';
 import './screens/orders_screen.dart';
 import './screens/cart_sсreen.dart';
 import './screens/product_detail_screen.dart';
-import './screens/products_overview_screen.dart';
 import './providers/cart_provider.dart';
 import './providers/products_provider.dart';
 import './providers/orders_provider.dart';
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
           accentColor: Colors.deepOrange,
           fontFamily: 'Lato',
         ),
-        home: ProductsOverviewScreen(),
+        home: AuthScreen(),
         routes: {
           ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
           CartScreen.routeName: (ctx) => CartScreen(),
